f8de97438b4e35ad7ced0a00fc8da31158dda811
wendtl <lwendt220@gmail.com>
2023-07-17 21:28:10 -0500

    add base app and homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
+import Home from "./pages/home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import "./styles/global.scss"
+
+
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menuContainer">
+            <Menu/>
+          </div>
+          <div className="contentContainer">
+            <Outlet/>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {
+          path: '/',
+          element: <Home/>
+        },
+        {
+          path: 'users/',
+          element: <Users/>
+        },
+        {
+          path: 'products/',
+          element: <Products/>
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login/>
+    }
+  ]);
+  
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
+
+
 
 export default App
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,1 +1,25 @@
-rfc
+import BarChartbox from '../../components/barChartbox/barChartbox';
+import Chartbox from '../../components/chartbox/Chartbox';
+import Topbox from '../../components/topbox/Topbox';
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxProduct, chartBoxUser } from '../../exampleData';
+import './home.scss'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="box box1">
+                <Topbox/>
+            </div>
+            <div className="box box2"><Chartbox {...chartBoxUser}/></div>
+            <div className="box box3"><Chartbox {...chartBoxProduct}/></div>
+            <div className="box box4">Box4</div>
+            <div className="box box5"><Chartbox {...chartBoxProduct}/></div>
+            <div className="box box6"><Chartbox {...chartBoxProduct}/></div>
+            <div className="box box7">Box7</div>
+            <div className="box box8"><BarChartbox {...barChartBoxVisit} /></div>
+            <div className="box box9"><BarChartbox {...barChartBoxRevenue} /></div>
+        </div>
+    );
+};
+
+export default Home;
