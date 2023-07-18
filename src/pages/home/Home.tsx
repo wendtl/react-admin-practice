e4a4441e318bc7028babc70be79a2a967bc7f1a9
wendtl <lwendt220@gmail.com>
2023-07-18 00:28:41 -0500

    add pie chart component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartbox from '../../components/barChartbox/barChartbox';
 import LineChartbox from '../../components/lineChartbox/LineChartbox';
+import PieChartbox from '../../components/pieChartbox/PieChartbox';
 import Topbox from '../../components/topbox/Topbox';
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxProduct, chartBoxUser } from '../../exampleData';
 import './home.scss'
@@ -12,10 +13,10 @@ const Home = () => {
             </div>
             <div className="box box2"><LineChartbox {...chartBoxUser} /></div>
             <div className="box box3"><LineChartbox {...chartBoxProduct} /></div>
-            <div className="box box4">Box4</div>
+            <div className="box box4"><PieChartbox /></div>
             <div className="box box5"><LineChartbox {...chartBoxProduct} /></div>
             <div className="box box6"><LineChartbox {...chartBoxProduct} /></div>
-            <div className="box box7">Box7</div>
+            <div className="box box7">box7</div>
             <div className="box box8"><BarChartbox {...barChartBoxVisit} /></div>
             <div className="box box9"><BarChartbox {...barChartBoxRevenue} /></div>
         </div>
